af1998c000b9fb4dc0efb136614336a2e1c58d99
Snoupix <samufpsdu59@hotmail.fr>
2024-05-24 18:15:11 +0200

    feat(weather): Impl weather display after fetch Impl memoization of the fetch call for caching Temporary disable of the geolocation watcher for compatibility purposes Update React & React-dom dependencies to latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-import { Weather } from "./view/Weather";
+import Loader from "./view/loader";
+import Weather from "./view/Weather";
 
 export default function App() {
+    const [geolocation, setGeolocation] = useState<GeolocationPosition | null>(null);
+    const [getPosError, setGetPosError] = useState<GeolocationPositionError | null>(null);
+
+    // useEffect(() => {
+    //     const watcher = navigator.geolocation.watchPosition(
+    //         position => setGeolocation(position),
+    //         error => setGetPosError(error),
+    //         {
+    //             timeout: 10000,
+    //             enableHighAccuracy: false,
+    //         }
+    //     );
+    //    
+    //     return () => navigator.geolocation.clearWatch(watcher);
+    // }, []);
+    useEffect(() => {
+        // TODO: The navigator watcher has some issues with my build of Firefox try to migrate it to a setInterval instead
+        navigator.geolocation.getCurrentPosition(
+            position => setGeolocation(position),
+            error => setGetPosError(error),
+            {
+                timeout: 10000,
+                enableHighAccuracy: false,
+            }
+        );
+    }, []);
+
+    useEffect(() => {
+        if (getPosError != null) {
+            console.error(getPosError);
+        }
+    }, [getPosError]);
+
 	return (
-		<main className="w-screen h-screen flex flex-col">
-			<Weather />
+		<main className="w-screen h-screen flex flex-col bg-emerald-800 text-sky-50 text-center font-semibold">
+            <section className="m-auto xl:w-3/12 w-8/12">
+                <h1 className="text-2xl mb-10">Simple weather app</h1>
+                {
+                    getPosError != null && getPosError.code != getPosError.TIMEOUT
+                        ? <h2>An unexpected error occured: {getPosError!.message}</h2>
+                        : null
+                }
+                {
+                    getPosError == null && geolocation != null
+                        ? <Weather lat={geolocation.coords.latitude} lon={geolocation.coords.longitude} time={new Date(geolocation.timestamp).toLocaleTimeString()} />
+                        : <Loader />
+                }
+            </section>
 		</main>
 	);
 }
---
+++ src/view/loader.tsx
@@ -0,0 +1,7 @@
+// TODO: I'm using my own loader from an other project, maybe change it later (role-playing as if it was a real project haha)
+export default function Loader() {
+    return <div className="flex flex-col gap-4 justify-center items-center">
+        <span>Loading...</span>
+        <div className="loader"></div>
+    </div>
+}
